5888cff482cf7a3da08070606e7785299f64cef7
ZawadulFarhan <zawadfarhan@gmail.com>
2016-09-10 14:04:40 +0800

    no message

--- www/js/controllers/HomeCtrl.js
+++ www/js/controllers/HomeCtrl.js
@@ -1,7 +1,46 @@
-﻿app.controller('HomeCtrl', function ($scope, $firebaseApp, $state, $user) {
+﻿app.controller('HomeCtrl', function ($scope, $firebaseApp, $state, $user, $ionicModal, $location) {
     if (!$user.isLoggedIn()) {
         $state.go('login');
     }
 
     $scope.user = $user;
+
+    $ionicModal.fromTemplateUrl('templates/receipt.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.receipt = modal;
+    });
+    $scope.openReceipt = function () {
+        $scope.receipt.show();
+    };
+    $scope.closeReceipt = function () {
+        $scope.receipt.hide();
+    };
+    // Cleanup the modal when we're done with it
+    $scope.$on('$destroy', function () {
+        $scope.receipt.remove();
+    });
+
+    $scope.getIdByQr = function () {
+
+        //TODO: invoke QR code
+        $state.go('payment');
+        //TODO: if correct, route to payment state
+
+        //TODO: invoke code confirmation method
+    };
+
+    $scope.getIdByText = function () {
+        $state.go('payment');
+        //TODO: invoke state/modal for input
+        //TODO: invoke code confirmation method
+    };
+
+    verifyCode = function () {
+        //TODO: if correct then go to payment page
+
+        //TODO: if wrong then show popover saying its wrong
+    };
+
 });
--- www/js/controllers/PaymentCtrl.js
+++ www/js/controllers/PaymentCtrl.js
@@ -1,3 +1,12 @@
 ﻿app.controller('PaymentCtrl', function ($scope, $firebaseApp, $ionicPopup, $state, $user) {
+    $scope.pay = function () {
+        //TODO: invoke method to send payment information
 
+        //TODO: if no error go back home
+        $state.go('app.home');
+
+        //TODO: else, show error message as popover
+    }
+
+    //TODO: have a scope variable storing store info
 });
